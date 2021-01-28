ae247e061af3ed017c37960b9db0df36d12078ff
M.Ivanov <milko.hristov.ivanov65@gmail.com>
2021-01-28 02:33:58 +0200

    Update get_whole.php

--- get_whole.php
+++ get_whole.php
@@ -1,25 +1,6 @@
 <?php
-require_once "client.php";
-$range = "A1:Z1000";
-$service = client::getInstance()->getService();
-$id = client::getInstance()->spreadsheetID;
-$response = $service->spreadsheets_values->get($id, $range);
-$values = $response->getValues();
-if (empty($values)){
-    echo "No values";
-}else{
-    $last = count($values[0]);
-    echo "<table><tr>";
-    foreach ($values[0] as $value) {
-        echo "<th>" . $value . "</th>";
-    }
-    echo "</tr>";
-    for ($i = 1;$i < $last;$i++){
-        echo "<tr>";
-        foreach ($values[$i] as $value) {
-            echo "<td>" . $value . "</td>";
-        }
-        echo "</tr>";
-    }
-    echo "</table>";
-}
+error_reporting(0);
+include "header.php";
+include_once "GetInRange.php";
+new GetInRange(true);
+include "footer.php";
